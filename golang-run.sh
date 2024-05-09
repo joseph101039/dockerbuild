fc2544ff6a04073b5f45d5a4f3b1893567598551
Joseph_Li <ptp_joseph_li@photons.com.tw>
2024-05-09 18:20:20 +0800

    update env

--- golang-run.sh
+++ golang-run.sh
@@ -16,16 +16,22 @@ fi
 # https://github.com/confluentinc/confluent-kafka-go/issues/454
 # 除錯模式: 安裝 dlv
 if [ "${DEBUG_PORT}" != "" ] &&  [ "${DEBUG_MODE}" != "0" ]; then
-  go install -v github.com/go-delve/delve/cmd/dlv@latest
+
+  if ! command -v dlv &> /dev/null
+  then
+      echo "dlv could not be found, installing dlv ..."
+      go install -v github.com/go-delve/delve/cmd/dlv@latest
+  fi
+
 
   echo "Ready to compile with debug mode."
   dlv version
   echo "Listen debug port ':${DEBUG_PORT}' at container"
+  echo "building golang executable files ..."
 
   # reference: https://www.jetbrains.com/help/go/attach-to-running-go-processes-with-debugger.html#step-1-build-the-application
   go build -tags musl -gcflags="all=-N -l" -o $GOPATH/bin/golang-dlv .
-  dlv --listen=":${DEBUG_PORT}" --headless=true --accept-multiclient --api-version=2 exec $GOPATH/bin/golang-dlv
-#  dlv debug --listen=":${DEBUG_PORT}" --headless=true --accept-multiclient --api-version=2 -- -tags musl -gcflags="all=-N -l"
+  dlv --listen=":${DEBUG_PORT}" --headless=true --accept-multiclient --api-version=2 --continue --log exec $GOPATH/bin/golang-dlv
 else
   go run -tags musl .
 fi
